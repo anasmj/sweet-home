bea15c706461b555431af2983bbd793bec6ef8a0
Anas Al Azmi <anasmj7@gmail.com>
2022-08-02 18:09:47 +0600

    fix: app bar

--- lib/screens/home_screen/home_page.dart
+++ lib/screens/home_screen/home_page.dart
@@ -16,11 +16,26 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       drawer: const Drawer(),
       body: DefaultTabController(
+        initialIndex: 1,
         length: 4,
         child: NestedScrollView(
           headerSliverBuilder: (context, value) {
             return [
               SliverAppBar(
+                //pinned: true,
+                title: Row(
+                  children: [
+                    const SizedBox(
+                      width: 250,
+                      child: Text(
+                        '232 West Nakhalpara',
+                        overflow: TextOverflow.fade,
+                        softWrap: false,
+                      ),
+                    ),
+                    Text(DateAndTime().currentDateAndMonth()),
+                  ],
+                ),
                 bottom: const TabBar(
                   tabs: [
                     Text('মাসিক হিসাব'),
@@ -29,203 +44,139 @@ class HomePage extends StatelessWidget {
                     Text('আমার খরচ'),
                   ],
                 ),
-                title: Row(
-                  children: [
-                    const Spacer(),
-                    Text(DateAndTime().currentDateAndMonth()),
-                  ],
-                ),
-                centerTitle: true,
-                expandedHeight: 280,
-                flexibleSpace: Column(
-                  children: <Widget>[
-                    const SizedBox(
-                      // upper space
-                      height: 10,
-                    ),
-                    Container(
-                      margin:
-                          const EdgeInsets.only(top: 80, left: 40, right: 40),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.6),
-                            blurRadius: 10,
-                            offset: const Offset(-2, 3),
-                          ),
-                        ],
-                        color: Colors.white,
-                      ),
-                      height: 160,
-                      //width: 50,
-                      child: Column(
-                        children: <Widget>[
-                          const SizedBox(
-                            // upper space
-                            height: 10,
-                          ),
-                          const Text(
-                            'এক নজরে',
-                            style: TextStyle(fontSize: 22),
-                          ),
-                          DataTable(
-                            dividerThickness: 3,
-                            dataTextStyle: const TextStyle(
-                                fontSize: 18, color: Colors.black),
-                            headingTextStyle: Theme.of(context)
-                                .textTheme
-                                .subtitle1!
-                                .copyWith(fontWeight: FontWeight.w600),
-                            columns: const <DataColumn>[
-                              DataColumn(
-                                label: Text('মোট পাবো'),
-                              ),
-                              DataColumn(
-                                label: Text('বুঝে পেয়েছি'),
-                              )
-                            ],
-                            rows: [
-                              DataRow(cells: <DataCell>[
-                                DataCell(
-                                  Row(
-                                    children: [
-                                      getIcon(AppIcons().takaUrl),
-                                      getAmountText(56000),
-                                    ],
-                                  ),
-                                ),
-                                DataCell(
-                                  Row(
-                                    children: [
-                                      getIcon(AppIcons().takaUrl),
-                                      getAmountText(12000),
-                                    ],
-                                  ),
-                                ),
-                              ]),
-                              // DataRow(
-                              //   label: Text('বুঝে পেয়েছি'),
-                              // )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
               ),
             ];
           },
-          body: const TabBarView(
-            children: [
-              Center(
-                child: Text('tab 1'),
-              ),
-              Center(
-                child: Text('tab 2'),
-              ),
-              Center(
-                child: Text('tab 3'),
-              ),
-              Center(
-                child: Text('tab 4'),
-              )
-            ],
-          ),
+          body: Container(),
         ),
       ),
-      // body: CustomScrollView(
-      //   slivers: [
-      //     SliverAppBar(
-      //       title: Row(
-      //         children: [
-      //           const Spacer(),
-      //           Text(DateAndTime().currentDateAndMonth()),
-      //         ],
-      //       ),
-      //       centerTitle: true,
-      //       expandedHeight: 260,
-      //       flexibleSpace: Column(
-      //         children: <Widget>[
-      //           const SizedBox(
-      //             // upper space
-      //             height: 10,
-      //           ),
-      //           Container(
-      //             margin: const EdgeInsets.only(top: 80, left: 40, right: 40),
-      //             decoration: BoxDecoration(
-      //               borderRadius: BorderRadius.circular(10),
-      //               boxShadow: [
-      //                 BoxShadow(
-      //                   color: Colors.black.withOpacity(0.6),
-      //                   blurRadius: 10,
-      //                   offset: const Offset(-2, 3),
-      //                 ),
-      //               ],
-      //               color: Colors.white,
-      //             ),
-      //             height: 160,
-      //             //width: 50,
-      //             child: Column(
-      //               children: <Widget>[
-      //                 const SizedBox(
-      //                   // upper space
-      //                   height: 10,
-      //                 ),
-      //                 const Text(
-      //                   'এক নজরে',
-      //                   style: TextStyle(fontSize: 22),
-      //                 ),
-      //                 DataTable(
-      //                   dividerThickness: 3,
-      //                   dataTextStyle:
-      //                       const TextStyle(fontSize: 18, color: Colors.black),
-      //                   headingTextStyle: Theme.of(context)
-      //                       .textTheme
-      //                       .subtitle1!
-      //                       .copyWith(fontWeight: FontWeight.w600),
-      //                   columns: const <DataColumn>[
-      //                     DataColumn(
-      //                       label: Text('মোট পাবো'),
-      //                     ),
-      //                     DataColumn(
-      //                       label: Text('বুঝে পেয়েছি'),
-      //                     )
-      //                   ],
-      //                   rows: [
-      //                     DataRow(cells: <DataCell>[
-      //                       DataCell(
-      //                         Row(
-      //                           children: [
-      //                             getIcon(AppIcons().takaUrl),
-      //                             getAmountText(56000),
-      //                           ],
-      //                         ),
-      //                       ),
-      //                       DataCell(
-      //                         Row(
-      //                           children: [
-      //                             getIcon(AppIcons().takaUrl),
-      //                             getAmountText(12000),
-      //                           ],
-      //                         ),
-      //                       ),
-      //                     ]),
-      //                     // DataRow(
-      //                     //   label: Text('বুঝে পেয়েছি'),
-      //                     // )
-      //                   ],
-      //                 ),
-      //               ],
-      //             ),
-      //           ),
-      //         ],
-      //       ),
-      //     )
-      //   ],
-      // ),
     );
+
+    // Widget build(BuildContext context) {
+    //   return Scaffold(
+    //     drawer: const Drawer(),
+    //     body: DefaultTabController(
+    //       length: 4,
+    //       child: NestedScrollView(
+    //         headerSliverBuilder: (context, value) {
+    //           return [
+    //             SliverAppBar(
+    //               bottom: const TabBar(
+    //                 tabs: [
+    //                   Text('মাসিক হিসাব'),
+    //                   Text('বকেয়াসমূহ'),
+    //                   Text('সকল গ্রাহক'),
+    //                   Text('আমার খরচ'),
+    //                 ],
+    //               ),
+    //               title: Row(
+    //                 children: [
+    //                   const Spacer(),
+    //                   Text(DateAndTime().currentDateAndMonth()),
+    //                 ],
+    //               ),
+    //               centerTitle: true,
+    //               expandedHeight: 280,
+    //               flexibleSpace: Column(
+    //                 children: <Widget>[
+    //                   const SizedBox(
+    //                     // upper space
+    //                     height: 10,
+    //                   ),
+    //                   Container(
+    //                     margin:
+    //                         const EdgeInsets.only(top: 80, left: 40, right: 40),
+    //                     decoration: BoxDecoration(
+    //                       borderRadius: BorderRadius.circular(10),
+    //                       boxShadow: [
+    //                         BoxShadow(
+    //                           color: Colors.black.withOpacity(0.6),
+    //                           blurRadius: 10,
+    //                           offset: const Offset(-2, 3),
+    //                         ),
+    //                       ],
+    //                       color: Colors.white,
+    //                     ),
+    //                     height: 160,
+    //                     //width: 50,
+    //                     child: Column(
+    //                       children: <Widget>[
+    //                         const SizedBox(
+    //                           // upper space
+    //                           height: 10,
+    //                         ),
+    //                         const Text(
+    //                           'এক নজরে',
+    //                           style: TextStyle(fontSize: 22),
+    //                         ),
+    //                         DataTable(
+    //                           dividerThickness: 3,
+    //                           dataTextStyle: const TextStyle(
+    //                               fontSize: 18, color: Colors.black),
+    //                           headingTextStyle: Theme.of(context)
+    //                               .textTheme
+    //                               .subtitle1!
+    //                               .copyWith(fontWeight: FontWeight.w600),
+    //                           columns: const <DataColumn>[
+    //                             DataColumn(
+    //                               label: Text('মোট পাবো'),
+    //                             ),
+    //                             DataColumn(
+    //                               label: Text('বুঝে পেয়েছি'),
+    //                             )
+    //                           ],
+    //                           rows: [
+    //                             DataRow(cells: <DataCell>[
+    //                               DataCell(
+    //                                 Row(
+    //                                   children: [
+    //                                     getIcon(AppIcons().takaUrl),
+    //                                     getAmountText(56000),
+    //                                   ],
+    //                                 ),
+    //                               ),
+    //                               DataCell(
+    //                                 Row(
+    //                                   children: [
+    //                                     getIcon(AppIcons().takaUrl),
+    //                                     getAmountText(12000),
+    //                                   ],
+    //                                 ),
+    //                               ),
+    //                             ]),
+    //                             // DataRow(
+    //                             //   label: Text('বুঝে পেয়েছি'),
+    //                             // )
+    //                           ],
+    //                         ),
+    //                       ],
+    //                     ),
+    //                   ),
+    //                 ],
+    //               ),
+    //             ),
+    //           ];
+    //         },
+    //         body: const TabBarView(
+    //           children: [
+    //             Center(
+    //               child: Text('tab 1'),
+    //             ),
+    //             Center(
+    //               child: Text('tab 2'),
+    //             ),
+    //             Center(
+    //               child: Text('tab 3'),
+    //             ),
+    //             Center(
+    //               child: Text('tab 4'),
+    //             )
+    //           ],
+    //         ),
+    //       ),
+    //     ),
+
+    //   );
   }
 
   Widget getIcon(String iconUrl) => SizedBox(
@@ -238,4 +189,43 @@ class HomePage extends StatelessWidget {
 
   //TODO: make it fancy by puttin comma between digits
   Widget getAmountText(int amount) => Text(' ${amount.toString()} /-');
+}
+
+class NormalAppBar extends StatelessWidget {
+  const NormalAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          title: Row(
+            children: [
+              const SizedBox(
+                width: 250,
+                child: Text(
+                  '232 West Nakhalpara',
+                  overflow: TextOverflow.fade,
+                  softWrap: false,
+                ),
+              ),
+              Text(DateAndTime().currentDateAndMonth()),
+            ],
+          ),
+          bottom: const TabBar(
+            tabs: [
+              Text('মাসিক হিসাব'),
+              Text('বকেয়াসমূহ'),
+              Text('সকল গ্রাহক'),
+              Text('আমার খরচ'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
